5dd2d236e297fa6ea8158c1dafcd65d0df605057
天爱有情 <tianaiyouqing@163.com>
2023-01-19 10:49:59 +0800

    适配1.4.0.1

--- src/main/java/cloud/tianai/captcha/spring/autoconfiguration/ImageCaptchaAutoConfiguration.java
+++ src/main/java/cloud/tianai/captcha/spring/autoconfiguration/ImageCaptchaAutoConfiguration.java
@@ -14,13 +14,11 @@ import cloud.tianai.captcha.spring.aop.CaptchaInterceptor;
 import cloud.tianai.captcha.spring.application.DefaultImageCaptchaApplication;
 import cloud.tianai.captcha.spring.application.ImageCaptchaApplication;
 import cloud.tianai.captcha.spring.plugins.SpringMultiImageCaptchaGenerator;
-import cloud.tianai.captcha.spring.plugins.chain.validators.DifferenceChainValidator;
 import cloud.tianai.captcha.spring.plugins.secondary.SecondaryVerificationApplication;
 import cloud.tianai.captcha.spring.store.CacheStore;
 import cloud.tianai.captcha.validator.ImageCaptchaValidator;
-import cloud.tianai.captcha.validator.impl.BasicCaptchaTrackValidator;
 import cloud.tianai.captcha.validator.impl.chain.ChainImageCaptchaValidator;
-import cloud.tianai.captcha.validator.impl.chain.validators.LRChainValidator;
+import cloud.tianai.captcha.validator.impl.chain.validators.BasicChainValidator;
 import cloud.tianai.captcha.validator.impl.chain.validators.ParamCheckChainValidator;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -83,10 +81,9 @@ public class ImageCaptchaAutoConfiguration {
     @Bean
     @ConditionalOnMissingBean
     public ImageCaptchaValidator imageCaptchaValidator() {
-        // 默认使用 LR 校验
         ChainImageCaptchaValidator imageCaptchaValidator = new ChainImageCaptchaValidator();
         imageCaptchaValidator.addValidator(new ParamCheckChainValidator());
-        imageCaptchaValidator.addValidator(new LRChainValidator());
+        imageCaptchaValidator.addValidator(new BasicChainValidator());
         return imageCaptchaValidator;
     }
 
